995137cd7c377d79d0789f33165f1b4a7793970a
Jonas Wolfram <jonaswolfram@Jonass-MBP.fritz.box>
2021-05-11 07:58:16 +0200

    Initial commit

---
+++ Modul_13_Structs_definieren_und_instanziieren/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul_13_Structs_definieren_und_instanziieren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
